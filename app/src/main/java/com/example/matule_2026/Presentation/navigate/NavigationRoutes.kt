f5c95c08eef5cd08f4b9c31881ba2b4d897ef7ff
Sergey Lyadov <ly4dov.s@yandex.ru>
2025-12-14 13:37:58 +0300

    Create navigation routes

--- app/src/main/java/com/example/matule_2026/Presentation/navigate/NavigationRoutes.kt
+++ app/src/main/java/com/example/matule_2026/Presentation/navigate/NavigationRoutes.kt
@@ -2,7 +2,27 @@ package com.example.matule_2026.Presentation.navigate
 
 object NavigationRoutes {
 
+    const val SPLASH = "splash"
 
+    const val SIGNINPIN = "singinpin"
 
+    const val SIGNINPASS = "singinpass"
 
+    const val CREATEPROFILE = "createprofile"
+
+    const val CREATEPASS = "createpass"
+
+    const val CREATEPIN = "createpin"
+
+    const val PROFILE = "profile"
+
+    const val MAIN = "main"
+
+    const val CATALOG = "catalog"
+
+    const val CART = "cart"
+
+    const val PROJECTS = "projects"
+
+    const val CREATEPROJECTS = "createprojects"
 }
